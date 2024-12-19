95967fba73af405363de51e4776c9c4dff46bf2d
unknown <your_email@example.com>
2024-12-19 15:18:09 +0600

    added page regions

--- src/app/regions/page.tsx
+++ src/app/regions/page.tsx
@@ -1,12 +1,14 @@
 import Region from '@/components/pages/regions/region/Region';
 import NavMap from '@/components/pages/regions/navMap/NavMap';
 import React from 'react';
+import Try from '@/components/pages/regions/try/Try';
 
 const RegionPage = () => {
     return (
         <>
             <Region/>
-            <NavMap />
+            {/* <NavMap /> */}
+            <Try />
         </>
     );
 };
