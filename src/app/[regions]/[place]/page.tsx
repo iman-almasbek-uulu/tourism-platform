d9684e0b09ca3e8f37b9e009c775971f0d974468
unknown <your_email@example.com>
2024-12-20 02:11:42 +0600

    added section popular places in region page

---
+++ src/app/[regions]/[place]/page.tsx
@@ -0,0 +1,17 @@
+"use client"
+import React from 'react';
+import { useParams, useRouter } from 'next/navigation';
+
+
+const PlacePage = () => {
+    const params = useParams()
+    console.log(params);
+    
+    
+    return (
+        <>
+        </>
+    );
+};
+
+export default PlacePage;
