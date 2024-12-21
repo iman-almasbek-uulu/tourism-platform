3a1b71db50d3b8986416ae3586c9e8643ca4cac6
munarabaisalova <munarabaisalova12@gmail.com>
2024-12-21 20:15:32 +0600

    fff

--- src/components/pages/popularPlace/reviews/Reviews.tsx
+++ src/components/pages/popularPlace/reviews/Reviews.tsx
@@ -4,14 +4,7 @@ import UseTranslate from "@/ui/Translate";
 const Reviews = () => {
     const {t} = UseTranslate()
     
-    const reviews = [
-        {
-            id: 1,
-            name: 'John Doe',
-            rating: 5,
-            review: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nec nulla vel purus tincidunt fermentum. Sed in turpis vel nunc tincidunt fermentum.' 
-        },
-    ]
+   
 }
 
 
