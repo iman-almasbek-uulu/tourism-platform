36932e673511e0cb4f1860541bea5369d834b6a5
KGstievK <kagastiev.kudaibergen.kk@gmail.com>
2025-01-07 15:21:56 +0600

    Update translate

--- src/components/pages/regions/region/Region.tsx
+++ src/components/pages/regions/region/Region.tsx
@@ -1,7 +1,7 @@
-"use client"
+"use client";
 import scss from './Region.module.scss';
-import imgBlock from "@/images/Rectangle 142.jpg"
-import groupPng from "@/images/Group.png"
+import imgBlock from "@/images/Rectangle 142.jpg";
+import groupPng from "@/images/Group.png";
 import UseTranslate from '@/ui/Translate';
 import { AppDispatch, RootState } from '@/redux/store';
 import { useDispatch, useSelector } from 'react-redux';
@@ -14,24 +14,29 @@ const Region = () => {
     const { t } = UseTranslate();
     const pathName = usePathname();
     const dispatch = useDispatch<AppDispatch>();
+    const router = useRouter();
     const { regions, error, loading } = useSelector((state: RootState) => state.regions);
-    const routeName = pathName.split("/")[1];    
+    const currentLang = useSelector((state: RootState) => state.language.currentLang);
+    const routeName = pathName.split("/")[1];
+
     useEffect(() => {
         dispatch(fetchRegions());
-    }, [dispatch]);
-    
-    const filteredRegion = regions.find((region: RegionData) => 
+    }, [dispatch, currentLang]); 
+    const filteredRegion = regions.find((region: RegionData) =>
         region.region_category.toLowerCase().replaceAll(" ", "") === routeName.toLowerCase().replaceAll(" ", "")
     );
 
-    
+    useEffect(() => {
+        if (!filteredRegion) {
+            router.push("/404");
+        }
+    }, [filteredRegion, router]);
 
     return (
         <>
             {loading && <h1>Loading...</h1>}
             {error && <h1>{error}</h1>}
             {filteredRegion ? (
-
                 <section id={scss.Region}>
                     <div className="container">
                         <div className={scss.region}>
@@ -39,22 +44,19 @@ const Region = () => {
                                 <img src={filteredRegion.region_image} alt="" />
                                 <div className="">
                                     <img src={groupPng.src} alt="temperature" />
-                                    <span>26^G</span>
+                                    <span>26°C</span>
                                 </div>
                             </div>
-                            <div className={scss.block}>    
-                                        <h2>{filteredRegion.region_name}</h2>
-                                        <p>{
-                                            filteredRegion.region_description.slice(0, 470) + "..."
-                                        }</p>
+                            <div className={scss.block}>
+                                <h2>{filteredRegion.region_name}</h2>
+                                <p>{filteredRegion.region_description.slice(0, 470) + "..."}</p>
                             </div>
                         </div>
                     </div>
                 </section>
-            )
-            : (                                
-                <p>{t("","","Region not found")}</p> // Сообщение, если регион не найден
-            )}        
+            ) : (
+                <p>{t("Регион не найден", "المنطقة غير موجودة", "Region not found")}</p>
+            )}
         </>
     );
 };
